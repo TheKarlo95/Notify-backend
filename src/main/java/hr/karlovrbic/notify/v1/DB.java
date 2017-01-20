fe0e67ec36051896d3d76d2f392b983213f89dfd
thekarlo95 <vrbic.karlo@gmail.com>
2017-01-20 15:51:55 +0100

    Fixed DB info

--- src/main/java/hr/karlovrbic/notify/v1/DB.java
+++ src/main/java/hr/karlovrbic/notify/v1/DB.java
@@ -5,11 +5,11 @@ package hr.karlovrbic.notify.v1;
  */
 public class DB {
 
-    public static final String DATABASE_NAME = "notifyDB";
+    public static final String DATABASE_NAME = "t9n6zvulavvuz4zl";
 
     public static final String TABLE_USER = "app_users";
     public static final String TABLE_EVENT = "event";
-    public static final String TABLE_USER_EVENT = "subscriptions";
+    public static final String TABLE_USER_EVENT = "user_subscriptions";
     public static final String TABLE_MESSAGE = "message";
     public static final String TABLE_COMMENT = "comment";
 }
